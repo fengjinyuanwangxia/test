6b32a1bc806f24c543a358f4547b514f8a536290
fengjinyuanwangxia <39868174+fengjinyuanwangxia@users.noreply.github.com>
2018-06-03 17:33:16 +0800

    Update day02.py

--- day02.py
+++ day02.py
@@ -5,6 +5,17 @@ Created on Sun Jun  3 09:41:11 2018
 @author: lenovo
 """
 import urllib.request as r
+
+print("欢迎使用小柚天气app")
+
+a=input("是否第一次登陆：")
+if a=="no":
+    print("登录成功")
+else:
+    print("欢迎第一次使用，请登记你的名字")
+    print("注册")
+
+
 city_pingyin=input("请输入城市拼音：")
 url='http://api.openweathermap.org/data/2.5/forecast?q={},cn&mode=json&lang=zh_cn&&APPID=6a67ed641c0fda8b69715c43518b6996'
 info=r.urlopen(url.format(city_pingyin)).read().decode('utf-8','ignore')
@@ -14,37 +25,20 @@ print(info)
 import json
 data=json.loads(info)
 
-print("欢迎使用小柚天气app")
-print("1.查看当前城市天气，2.查看未来五天的天气")
+
+print("1.查看未来五天的天气,2.查看其他城市天气,3.保存")
 
 menno=input("请输入菜单：")
 
+    
 if menno=="1":
-    print("查看当前城市天气")
-    a=data['main']['temp']
-    b=data['weather'][0]["description"]
-    c=data['main']['temp_max']
-    d=data['main']['pressure']
-    print("温度："+str(a))
-    print("天气情况:"+b)
-    print("最高温度："+str(c))
-    print("气压："+str(d))
-    
-if menno=="2":
     print("未来五天的天气:")
-    print("日期："+str(data["list"][0]["dt_txt"]))
-    print(str(data["list"][0]['weather'][0]['description'])+";最高温度为:"+str(data["list"][0]["main"]["temp_max"])+";最低温度为:"+str(data["list"][0]["main"]["temp_min"]))
-    
-    print("日期："+str(data["list"][8]["dt_txt"]))
-    print(str(data["list"][8]['weather'][0]['description'])+";最高温度为:"+str(data["list"][8]["main"]["temp_max"])+";最低温度为:"+str(data["list"][8]["main"]["temp_min"]))
-    
-    print("日期："+str(data["list"][16]["dt_txt"]))
-    print(str(data["list"][16]['weather'][0]['description'])+";最高温度为:"+str(data["list"][16]["main"]["temp_max"])+";最低温度为:"+str(data["list"][16]["main"]["temp_min"]))
-      
-    print("日期："+str(data["list"][24]["dt_txt"]))
-    print(str(data["list"][24]['weather'][0]['description'])+";最高温度为:"+str(data["list"][24]["main"]["temp_max"])+";最低温度为:"+str(data["list"][24]["main"]["temp_min"]))
-      
-    print("日期："+str(data["list"][32]["dt_txt"]))
-    print(str(data["list"][32]['weather'][0]['description'])+";最高温度为:"+str(data["list"][32]["main"]["temp_max"])+";最低温度为:"+str(data["list"][32]["main"]["temp_min"]))
-   
-    
+    for i in range(0,36):
+        print("日期："+str(data["list"][i]["dt_txt"]))
+        print(str(data["list"][i]['weather'][0]['description'])+";最高温度为:"+str(data["list"][i]["main"]["temp_max"])+";最低温度为:"+str(data["list"][i]["main"]["temp_min"]))
+
+if menno=="2":
+    print("查看其他城市天气")
+
+if menno=="3":
+    print("保存")
